0e64747402141a02724668505ad8a65416419481
igorxciv <icheliadinski@gmail.com>
2021-11-07 22:28:30 -0500

    new: add commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	extends: ['@commitlint/config-conventional'],
+	rules: {
+		'type-enum': [2, 'always', ['chg', 'new', 'fix']],
+		'scope-case': [2, 'always', 'upper-case'],
+	},
+}
