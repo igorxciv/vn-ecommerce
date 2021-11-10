9186d7295510830d3074cc0ea0f15ac9d0a04995
igorxciv <icheliadinski@gmail.com>
2021-11-09 22:42:08 -0500

    new: add pages

--- apps/frontend/src/app/routable-content/RoutableContent.tsx
+++ apps/frontend/src/app/routable-content/RoutableContent.tsx
@@ -1,3 +1,22 @@
-export const RoutableContent = () => {
-	return <main>some content</main>;
+import classNames from 'classnames/bind';
+import { FC } from 'react';
+import { Redirect, Route, Switch } from 'react-router-dom';
+import styles from './RoutableContent.module.scss';
+import { Home } from '../../pages/home';
+import { Cart } from '../../pages/cart';
+
+const cx = classNames.bind(styles);
+
+export const RoutableContent: FC = () => {
+	const contentClasses = cx('content');
+
+	return (
+		<main className={contentClasses}>
+			<Switch>
+				<Route path="/" component={Home} exact />
+				<Route path="/cart" component={Cart} />
+				<Redirect to="/" />
+			</Switch>
+		</main>
+	);
 };
---
+++ apps/frontend/src/pages/cart/Cart.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+
+export const Cart: FC = () => {
+	return (
+		<div>
+			<h1>Cart</h1>
+		</div>
+	);
+};
