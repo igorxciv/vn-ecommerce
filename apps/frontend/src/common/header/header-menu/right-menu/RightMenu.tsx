a518fd9c1a3f7b4f722cad9d421738277ac21f6e
igorxciv <icheliadinski@gmail.com>
2021-11-10 22:24:00 -0500

    new: add user icon

--- apps/frontend/src/common/header/header-menu/right-menu/RightMenu.tsx
+++ apps/frontend/src/common/header/header-menu/right-menu/RightMenu.tsx
@@ -2,17 +2,22 @@ import { FC } from 'react';
 import classNames from 'classnames/bind';
 import { Link } from 'react-router-dom';
 import { ReactComponent as ShoppingBagIcon } from '@vn-ecommerce/ui-kit/icons/shopping-bag.svg';
+import { ReactComponent as UserIcon } from '@vn-ecommerce/ui-kit/icons/user.svg';
 import styles from './RightMenu.module.scss';
 
 const cx = classNames.bind(styles);
 
 export const RightMenu: FC = () => {
 	const rightMenuClasses = cx('right-menu');
-	const rightMenuLinkClasses = cx('right-menu__link');
+	const linkClasses = cx('right-menu__link');
+	const userLinkClasses = cx(linkClasses, 'right-menu__user');
 
 	return (
 		<div className={rightMenuClasses}>
-			<Link to="/cart" className={rightMenuLinkClasses}>
+			<Link to="/account" className={userLinkClasses}>
+				<UserIcon />
+			</Link>
+			<Link to="/cart" className={linkClasses}>
 				<ShoppingBagIcon />
 			</Link>
 		</div>
