f69822c26ecca559045800c195be269093e3517b
igorxciv <icheliadinski@gmail.com>
2021-11-13 11:51:39 -0500

    fix: fix jumping header

--- apps/frontend/src/common/header/Header.tsx
+++ apps/frontend/src/common/header/Header.tsx
@@ -1,4 +1,5 @@
 import classNames from 'classnames/bind';
+import { Transition } from 'react-transition-group';
 import { useScrollDirectionChange, useTopState } from '@frontend/libs/scroll';
 import styles from './Header.module.scss';
 import { HeaderMenu } from './header-menu';
@@ -16,13 +17,17 @@ export const Header = () => {
 	const navigationClasses = cx('header__navigation');
 
 	return (
-		<header className={headerClasses}>
-			<div className={menuClasses}>
-				<HeaderMenu />
-			</div>
-			<div className={navigationClasses}>
-				<NavigationMenu compact={compact} />
-			</div>
-		</header>
+		<Transition in={compact} timeout={0}>
+			{(state) => (
+				<header className={cx(headerClasses, `header--${state}`)}>
+					<div className={menuClasses}>
+						<HeaderMenu />
+					</div>
+					<div className={navigationClasses}>
+						<NavigationMenu compact={compact} />
+					</div>
+				</header>
+			)}
+		</Transition>
 	);
 };
