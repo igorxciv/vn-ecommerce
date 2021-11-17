1c375c4de38c427734aca6256d396382b4d0900e
igorxciv <icheliadinski@gmail.com>
2021-11-16 22:49:26 -0500

    new: add styles for filters panel

--- apps/frontend/src/pages/tabs/filters-panel/FiltersPanel.tsx
+++ apps/frontend/src/pages/tabs/filters-panel/FiltersPanel.tsx
@@ -2,12 +2,16 @@ import { FC } from 'react';
 import { DropdownButton } from '@vn-ecommerce/ui-kit/atoms/dropdown-button';
 import { useTranslation } from 'react-i18next';
 import classNames from 'classnames/bind';
+import { useScrollDirectionChange, useTopState } from '@frontend/libs/scroll';
 import styles from './FiltersPanel.module.scss';
 
 const cx = classNames.bind(styles);
 
 export const FiltersPanel: FC = () => {
 	const { t } = useTranslation('common');
+	const isTop = useTopState();
+	const direction = useScrollDirectionChange();
+	const compact = direction === 'down';
 
 	const handleFilterClick = () => {
 		console.log('filter click');
@@ -17,12 +21,15 @@ export const FiltersPanel: FC = () => {
 		console.log('sorting click');
 	};
 
-	const panelClasses = cx('filters-panel');
+	const panelClasses = cx('filters-panel', { 'filters-panel--top': isTop, 'filters-panel--compact': compact });
+	const innerClasses = cx('filters-panel__inner');
 
 	return (
 		<div className={panelClasses}>
-			<DropdownButton onClick={handleFilterClick}>{t('filters.filter')}</DropdownButton>
-			<DropdownButton onClick={handleSortingClick}>{t('filters.sorting')}</DropdownButton>
+			<div className={innerClasses}>
+				<DropdownButton onClick={handleFilterClick}>{t('filters.filter')}</DropdownButton>
+				<DropdownButton onClick={handleSortingClick}>{t('filters.sorting')}</DropdownButton>
+			</div>
 		</div>
 	);
 };
