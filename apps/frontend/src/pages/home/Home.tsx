9d1088fd8727e805d3f9aec96690ba4e24c02176
igorxciv <icheliadinski@gmail.com>
2021-11-20 10:55:59 -0500

    new: add titles for homepage

--- apps/frontend/src/pages/home/Home.tsx
+++ apps/frontend/src/pages/home/Home.tsx
@@ -3,6 +3,7 @@ import { useTranslation } from 'react-i18next';
 import { useTitle } from '@frontend/libs/title';
 import { Video } from '@vn-ecommerce/ui-kit/atoms/video';
 import classNames from 'classnames/bind';
+import { Button } from '@vn-ecommerce/ui-kit/atoms/button';
 import styles from './Home.module.scss';
 
 const cx = classNames.bind(styles);
@@ -13,82 +14,16 @@ export const Home: FC = () => {
 	useTitle(`${t('logo1')} ${t('logo2')} | ${t('page_title')}`);
 
 	const homeClasses = cx('home');
+	const titleClasses = cx('home__title');
+	const subTitleClasses = cx('home__sub-title');
+	const selectCourseClasses = cx('home__select-course');
 
 	return (
 		<div className={homeClasses}>
 			<Video src="http://localhost:3000/videos/venera/venera.m3u8" />
-			<h1>Home</h1>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
-			<div>1</div>
+			<h1 className={titleClasses}>{t('pages.home.main_title')}</h1>
+			<h5 className={subTitleClasses}>{t('pages.home.sub_title')}</h5>
+			<Button className={selectCourseClasses}>{t('pages.home.select_course')}</Button>
 		</div>
 	);
 };
