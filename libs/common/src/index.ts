8182a2b507bf82f68bd0bb55b3d61b9e11577235
igorxciv <icheliadinski@gmail.com>
2021-12-11 22:15:55 -0500

    new: add logger

--- libs/common/src/index.ts
+++ libs/common/src/index.ts
@@ -3,3 +3,4 @@ export * from './lib/filters';
 export * from './lib/decorators';
 export * from './lib/guards';
 export * from './lib/interceptors';
+export * from './lib/middlewares';
--- libs/common/src/lib/common.module.ts
+++ libs/common/src/lib/common.module.ts
@@ -1,10 +1,15 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 import { UserGuard } from './guards';
+import { LoggingMiddleware } from './middlewares';
 
 @Module({
 	controllers: [],
 	providers: [{ provide: APP_GUARD, useClass: UserGuard }],
 	exports: [],
 })
-export class CommonModule {}
+export class CommonModule implements NestModule {
+	configure(consumer: MiddlewareConsumer) {
+		consumer.apply(LoggingMiddleware).forRoutes('*');
+	}
+}
