f7ae9b97d6cf8b5c48af11bbd0bbc677afbcc226
igorxciv <icheliadinski@gmail.com>
2021-12-26 08:24:48 -0500

    new: add swager api property

--- libs/models/src/lib/dto/pagination-query.dto.ts
+++ libs/models/src/lib/dto/pagination-query.dto.ts
@@ -1,10 +1,13 @@
 import { IsOptional, IsPositive } from 'class-validator';
+import { ApiPropertyOptional } from '@nestjs/swagger';
 
 export class PaginationQueryDto {
+	@ApiPropertyOptional({ description: 'Max amount of products request should return', example: 20 })
 	@IsOptional()
 	@IsPositive()
 	limit: number;
 
+	@ApiPropertyOptional({ description: 'Offset of products request', example: 5 })
 	@IsOptional()
 	@IsPositive()
 	offset: number;
